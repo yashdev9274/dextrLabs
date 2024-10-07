fd22904c15110b1cbae6d2c9ebf7a6295eb619bf
yashdev9274 <yashdev.yvd@gmail.com>
2024-10-07 14:45:38 +0530

    update: new env for production, gitignore.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  resctrictionMode: true,
   images:{
     domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   }
 }
 
